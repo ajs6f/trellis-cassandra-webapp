29513c36a75a8f66b8547ff212db38d302d0852f
ajs6f <ajs6f@apache.org>
2018-03-21 13:44:37 -0400

    Correct behavior for binaries/bitstreams

--- src/main/java/edu/si/trellis/cassandra/TrellisCassandraApplication.java
+++ src/main/java/edu/si/trellis/cassandra/TrellisCassandraApplication.java
@@ -19,8 +19,10 @@ public class TrellisCassandraApplication extends TrellisApplication {
     @Override
     protected <T extends ResourceService> T buildResourceService(IdentifierService idService,
                     MementoService mementoService, EventService notificationService) {
-        Cluster cluster = Cluster.builder().withoutJMXReporting().withoutMetrics().addContactPoint("127.0.0.1")
-                        .withPort(9042).build();
+        String cassandraAddress = config.getCassandraAddress();
+        Integer cassandraPort = config.getCassandraPort();
+        Cluster cluster = Cluster.builder().withoutJMXReporting().withoutMetrics().addContactPoint(cassandraAddress)
+                        .withPort(cassandraPort).build();
         cluster.getConfiguration().getCodecRegistry().register(iriCodec, datasetCodec, bigint(), InstantCodec.instance);
         Session session = cluster.connect("Trellis");
         return (T) new CassandraResourceService(session);
