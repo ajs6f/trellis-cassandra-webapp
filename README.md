a016bdd33baab23260951530aa34644cd44839e7
ajs6f <ajs6f@apache.org>
2018-03-21 10:45:51 -0400

    First draft

---
+++ README.md
@@ -0,0 +1,3 @@
+A Trellis LDP application using Trellis-Cassandra for persistence.
+
+See [Trellis-Cassandra](https://github.com/ajs6f/trellis-cassandra) and [Trellis](https://github.com/trellis-ldp/trellis).
---
+++ src/main/java/edu/si/trellis/cassandra/TrellisCassandraApplication.java
@@ -0,0 +1,39 @@
+package edu.si.trellis.cassandra;
+
+import static com.datastax.driver.core.TypeCodec.bigint;
+import static edu.si.trellis.cassandra.DatasetCodec.datasetCodec;
+import static edu.si.trellis.cassandra.IRICodec.iriCodec;
+
+import org.trellisldp.api.EventService;
+import org.trellisldp.api.IdentifierService;
+import org.trellisldp.api.MementoService;
+import org.trellisldp.api.ResourceService;
+import org.trellisldp.app.TrellisApplication;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
+import com.datastax.driver.extras.codecs.jdk8.InstantCodec;
+
+public class TrellisCassandraApplication extends TrellisApplication {
+
+    @Override
+    protected <T extends ResourceService> T buildResourceService(IdentifierService idService,
+                    MementoService mementoService, EventService notificationService) {
+        Cluster cluster = Cluster.builder().withoutJMXReporting().withoutMetrics().addContactPoint("127.0.0.1")
+                        .withPort(9042).build();
+        cluster.getConfiguration().getCodecRegistry().register(iriCodec, datasetCodec, bigint(), InstantCodec.instance);
+        Session session = cluster.connect("Trellis");
+        return (T) new CassandraResourceService(session);
+    }
+
+    /**
+     * The main entry point.
+     * 
+     * @param args the argument list
+     * @throws Exception if something goes horribly awry
+     */
+    public static void main(final String[] args) throws Exception {
+        new TrellisCassandraApplication().run(args);
+    }
+
+}
