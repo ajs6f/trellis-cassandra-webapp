b60b4706dcc83fd0f0b2ed77ee97898db4b550d0
ajs6f <ajs6f@apache.org>
2018-04-03 11:21:07 -0400

    Aligning tests with new Trellis test arrangement

--- src/test/java/edu/si/trellis/cassandra/TrellisCassandraLdpIT.java
+++ src/test/java/edu/si/trellis/cassandra/TrellisCassandraLdpIT.java
@@ -1,40 +1,57 @@
 package edu.si.trellis.cassandra;
 
+import static org.glassfish.jersey.client.ClientProperties.CONNECT_TIMEOUT;
+import static org.glassfish.jersey.client.ClientProperties.READ_TIMEOUT;
+
 import io.dropwizard.client.JerseyClientBuilder;
 import io.dropwizard.testing.DropwizardTestSupport;
 
-import java.io.File;
+import javax.ws.rs.client.Client;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
 import org.trellisldp.app.TrellisApplication;
 import org.trellisldp.app.config.TrellisConfiguration;
-import org.trellisldp.test.LdpTests;
+import org.trellisldp.test.AbstractApplicationLdpTests;
 
-public class TrellisCassandraLdpIT extends LdpTests {
+
+@RunWith(JUnitPlatform.class)
+public class TrellisCassandraLdpIT extends AbstractApplicationLdpTests {
 
     private static final String BUILD_DIR = System.getProperty("project.build.directory", "");
 
     private static final DropwizardTestSupport<TrellisConfiguration> APP = new DropwizardTestSupport<TrellisConfiguration>(
                     TrellisApplication.class, BUILD_DIR + "/test-classes/trellis-config.yml");
 
+    private static Client CLIENT;
+
+    @Override
+    public Boolean supportWebAnnotationProfile() {
+        return false;
+    }
+
     @BeforeAll
     public static void initialize() throws Exception {
         APP.before();
-        setClient(new JerseyClientBuilder(APP.getEnvironment()).build("test client"));
-        setBaseUrl("http://localhost:" + APP.getLocalPort() + "/");
-
-        setUp();
-    }
-
-    @Override
-    protected String getResourcePath(String path) {
-        return new File(BUILD_DIR + "/test-classes/" + path).getAbsolutePath();
+        CLIENT = new JerseyClientBuilder(APP.getEnvironment()).build("test client");
+        CLIENT.property(CONNECT_TIMEOUT, 5000);
+        CLIENT.property(READ_TIMEOUT, 5000);
     }
 
     @AfterAll
     public static void cleanup() throws Exception {
         APP.after();
-        tearDown();
+    }
+
+    @Override
+    public Client getClient() {
+        return CLIENT;
+    }
+
+    @Override
+    public String getBaseURL() {
+        return "http://localhost:" + APP.getLocalPort() + "/";
     }
 }
