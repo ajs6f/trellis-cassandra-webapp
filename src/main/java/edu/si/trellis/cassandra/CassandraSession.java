4caae2c3ad7ed79bcdd24cff533e873a41d5f025
ajs6f <ajs6f@apache.org>
2018-09-04 10:31:05 -0400

    Now with JAX-RS, CDI _and_ Tamaya

--- src/main/java/edu/si/trellis/cassandra/CassandraSession.java
+++ src/main/java/edu/si/trellis/cassandra/CassandraSession.java
@@ -3,39 +3,24 @@ package edu.si.trellis.cassandra;
 import static com.datastax.driver.core.TypeCodec.bigint;
 import static edu.si.trellis.cassandra.DatasetCodec.datasetCodec;
 import static edu.si.trellis.cassandra.IRICodec.iriCodec;
-import static org.apache.tamaya.ConfigurationProvider.createConfiguration;
-import static org.apache.tamaya.ConfigurationProvider.getConfiguration;
-import static org.apache.tamaya.ConfigurationProvider.setConfiguration;
-import static org.apache.tamaya.format.ConfigurationFormats.createPropertySource;
+import static java.lang.Integer.parseInt;
 import static org.slf4j.LoggerFactory.getLogger;
 
 import com.datastax.driver.core.Cluster;
-import com.datastax.driver.core.QueryLogger;
 import com.datastax.driver.core.Session;
 import com.datastax.driver.extras.codecs.jdk8.InstantCodec;
 
 import java.io.Closeable;
-import java.io.IOException;
-import java.io.UncheckedIOException;
-import java.net.URL;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.context.Dependent;
 import javax.enterprise.inject.Produces;
-import javax.enterprise.inject.spi.CDI;
 import javax.inject.Inject;
 
-import org.apache.tamaya.Configuration;
-import org.apache.tamaya.ConfigurationProvider;
-import org.apache.tamaya.cdi.TamayaCDIInjectionExtension;
-import org.apache.tamaya.format.ConfigurationFormats;
 import org.apache.tamaya.inject.api.Config;
-import org.apache.tamaya.spi.ConfigurationContext;
-import org.apache.tamaya.spi.PropertySource;
-import org.apache.tamaya.json.JSONFormat;
+import org.apache.tamaya.inject.api.ConfigDefaultSections;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @ApplicationScoped
 public class CassandraSession implements Closeable {
@@ -43,48 +28,25 @@ public class CassandraSession implements Closeable {
     private Cluster cluster;
 
     private Session session;
-    
-    @Config
-    @Inject
-    private String cassandraHostAddy;
-    
-    @Config(required=false)
-    @Inject
-    private Integer cassandraPort;
 
-    //@Inject
-    //TamayaCDIInjectionExtension j;
+    @Inject
+    @Config
+    private String contactPort;
+
+    @Inject
+    @Config
+    private String contactAddress;
 
     private static final Logger log = getLogger(CassandraSession.class);
-
-    static {
-
-        // try {
-        // URL configResource = CassandraSession.class.getClassLoader().getResource("trellis-config.yml");
-        // PropertySource config = createPropertySource(configResource, new YAMLFormat());
-        // ConfigurationContext context = getConfiguration().getContext();
-        // context = context.toBuilder().addPropertySources(config).build();
-        // setConfiguration(createConfiguration(context));
-        // log.debug("Installed YAML configuration to Tamaya.");
-        // } catch (IOException e) {
-        // throw new UncheckedIOException(e);
-        // }
-    }
-
-    public CassandraSession() {
-        //log.debug("Found TamayaCDIInjectionExtension: {}", j);
-        //log.debug("System properties:");
-        //System.getProperties().forEach((k,v)->log.debug("{} -> {}", k, v));
-        String cassandraAddress = "localhost";
-        int port = Integer.getInteger("cassandra.nativeTransportPort");
-        log.info("Found config'd cassandra node address: {} and port: {}", cassandraHostAddy, cassandraPort);
-        log.info("Using cassandra node address: {} and port: {}", cassandraAddress, port);
-        
-        this.cluster = Cluster.builder().withoutJMXReporting().withoutMetrics().addContactPoint(cassandraAddress)
-                        .withPort(port).build();
-        QueryLogger queryLogger = QueryLogger.builder().build();
-        this.cluster.register(queryLogger);
-        this.cluster.getConfiguration().getCodecRegistry().register(iriCodec, datasetCodec, bigint(), InstantCodec.instance);
+    
+    @PostConstruct
+    public void connect() {
+        log.info("Using Cassandra node address: {} and port: {}", contactAddress, contactPort);
+        this.cluster = Cluster.builder().withoutJMXReporting().withoutMetrics().addContactPoint(contactAddress)
+                        .withPort(parseInt(contactPort)).build();
+        // this.cluster.register(QueryLogger.builder().build());
+        this.cluster.getConfiguration().getCodecRegistry().register(iriCodec, datasetCodec, bigint(),
+                        InstantCodec.instance);
         this.session = cluster.connect("Trellis");
     }
 
